8a9870a2991cc276659cb3851f111929dbafd869
MK <mahmoud.k.nasr@gmail.com>
2017-11-26 20:41:15 +0200

    include Dest Fetching Block in the CU

--- CU.vhd
+++ CU.vhd
@@ -5,7 +5,6 @@ USE IEEE.NUMERIC_STD.ALL;
 USE WORK.MY_PACKAGE.ALL;
 
 ENTITY CU IS
-	GENERIC (MODE_BITS_NUM: integer := 3);
 	PORT( --CONTROL_CLK :IN std_logic;
 		IR : IN std_logic_vector (IR_SIZE-1 DOWNTO 0);
 		FLAG_REGISTER : IN std_logic_vector (3 DOWNTO 0));
@@ -36,20 +35,48 @@ ARCHITECTURE CONTROL_UNIT OF CU IS
 		INITIAL_ADDRESS: OUT STD_LOGIC_VECTOR (INSTRUCTION_COUNTER_OUTPUT_SIZE-1 DOWNTO 0) );
 	END COMPONENT SRC_FETCHING_BLOCK;
 	
+	COMPONENT DEST_FETCHING_BLOCK IS
+	PORT (
+		IR : IN std_logic_vector (IR_SIZE-1 DOWNTO 0);
+		INSTRUCTION_COUNTER : IN STD_LOGIC_VECTOR (INSTRUCTION_COUNTER_OUTPUT_SIZE-1 DOWNTO 0); 
+		CONTROL_WORD: OUT STD_LOGIC_VECTOR (ROM_WIDTH-1 DOWNTO 0);
+		INITIAL_ADDRESS: OUT STD_LOGIC_VECTOR (INSTRUCTION_COUNTER_OUTPUT_SIZE-1 DOWNTO 0) );
+	END COMPONENT DEST_FETCHING_BLOCK;
+	--CLK SIGNAL
 	SIGNAL CONTROL_CLK : std_logic;
 	--TODO set NEW_INSTRUCTION bit at the end of every instruction and clear it at the start of every instruction TO RESET THE CIRCUIT
+	------------------------COUNTERS SIGNALS-------------------
+	--RESET THE MAIN COUNTER
 	SIGNAL NEW_INSTRUCTION : std_logic := '0'; 
 	SIGNAL ENABLE_MAIN_COUNTER : std_logic := '1';
 	SIGNAL ENABLE_INSTRUCTION_COUNTER : std_logic := '0';
-	SIGNAL INITIAL_ADDRESS : std_logic_vector(INSTRUCTION_COUNTER_OUTPUT_SIZE-1 DOWNTO 0) := "000";
-	SIGNAL MODE : std_logic_vector(MODE_BITS_NUM-1 DOWNTO 0);
-	SIGNAL MAIN_COUNTER_OUTPUT, INSTRUCTION_COUNTER_OUTPUT: std_logic_vector (INSTRUCTION_COUNTER_OUTPUT_SIZE-1 DOWNTO 0);
+	SIGNAL MAIN_COUNTER_OUTPUT: std_logic_vector (MAIN_COUNTER_OUTPUT_SIZE-1 DOWNTO 0); 
+	SIGNAL INSTRUCTION_COUNTER_OUTPUT: std_logic_vector (INSTRUCTION_COUNTER_OUTPUT_SIZE-1 DOWNTO 0);
 	SIGNAL MAIN_COUNTER_INCREMENT : std_logic_vector (1 DOWNTO 0);
-	SIGNAL ROM_BLOCKS_OUTPUT : STD_LOGIC_VECTOR (ROM_WIDTH-1 DOWNTO 0);
+	SIGNAL INITIAL_ADDRESS : std_logic_vector(INSTRUCTION_COUNTER_OUTPUT_SIZE-1 DOWNTO 0) := "000";
+	
+	--OPERATION MODE SIGNAL
+	SIGNAL MODE : std_logic_vector(MODE_BITS_NUM-1 DOWNTO 0);
+	---------------------ROMS Block Signals------------------
+	--ROMs Blocks CODE WORD Output
+	SIGNAL ROM_BLOCKS_OUTPUT, INSTRUCTION_FETCHING_BLOCK_OUTPUT
+		, SRC_FETCHING_BLOCK_OUTPUT
+		, DEST_FETCHING_BLOCK_OUTPUT, EXECUTE_BLOCK_OUTPUT
+		, EXECUTE_BRANCH_BLOCK_OUTPUT : STD_LOGIC_VECTOR (ROM_WIDTH-1 DOWNTO 0) := (OTHERS =>'0');
+	--ROMs Blocks INITIAL ADDRESS Output
+	SIGNAL INSTRUCTION_FETCHING_BLOCK_INITIAL_ADDRESS_OUTPUT
+		, SRC_FETCHING_BLOCK_INITIAL_ADDRESS_OUTPUT, DEST_FETCHING_BLOCK_INITIAL_ADDRESS_OUTPUT
+		, EXECUTE_BLOCK_INITIAL_ADDRESS_OUTPUT
+		, EXECUTE_BRANCH_BLOCK_INITIAL_ADDRESS_OUTPUT : STD_LOGIC_VECTOR (INSTRUCTION_COUNTER_OUTPUT_SIZE-1 DOWNTO 0);
 	BEGIN
 	-------------------------------Port Mapping-----------------------------------------------
 	------------------------------------------------------------------------------------------
 	--Counters
+	-- MAIN_COUNTER_OUTPUT 	= 0 FETCH INSTRUCTION
+	--					   	= 1 FETCH SRC
+	--					   	= 2 FETCH DEST
+	--						= 3 EXECUTE ( 1, 2 OR ZERO OPRAND INSTRUCTIONS)
+	--						= 4 EXECUTE BRANCH INSTRUCTION 
 	MAIN_COUNTER: NBIT_COUNTER_WITH_INCREMENT 
 		GENERIC MAP (N => MAIN_COUNTER_OUTPUT_SIZE) 
 		PORT MAP ( CLK => CONTROL_CLK , RST => NEW_INSTRUCTION, ENABLE => ENABLE_MAIN_COUNTER
@@ -61,7 +88,10 @@ ARCHITECTURE CONTROL_UNIT OF CU IS
 	
 	--ROMs Blocks
 	SRC_FETCH: SRC_FETCHING_BLOCK PORT MAP (IR => IR, INSTRUCTION_COUNTER => INSTRUCTION_COUNTER_OUTPUT
-		, CONTROL_WORD => ROM_BLOCKS_OUTPUT, INITIAL_ADDRESS => INITIAL_ADDRESS);
+		, CONTROL_WORD => SRC_FETCHING_BLOCK_OUTPUT, INITIAL_ADDRESS => SRC_FETCHING_BLOCK_INITIAL_ADDRESS_OUTPUT);
+		
+	DEST_FETCH: DEST_FETCHING_BLOCK PORT MAP (IR => IR, INSTRUCTION_COUNTER => INSTRUCTION_COUNTER_OUTPUT
+		, CONTROL_WORD => DEST_FETCHING_BLOCK_OUTPUT, INITIAL_ADDRESS => DEST_FETCHING_BLOCK_INITIAL_ADDRESS_OUTPUT);
 	-------------------------------Creating the control clk-----------------------------------
 	------------------------------------------------------------------------------------------
 	PROCESS
@@ -76,11 +106,45 @@ ARCHITECTURE CONTROL_UNIT OF CU IS
 	----------------------------------Logic---------------------------------------------------
 	------------------------------------------------------------------------------------------
 	
+	------------------------------------------------------------------------------------------
+	-------------------------------Decode the IR for the MODE----------------------------
+	-- Mode = 0 the IR wasn't fetched yet
+	--		= 1 2 operands
+	--		= 2 1 operands
+	--		= 3 zero operand
+	--		= 4 branch
+	MODE <= TWO_OPERAND WHEN NOT (IR(IR_SIZE-1 DOWNTO 12) = X"0" OR IR(IR_SIZE-1 DOWNTO 12) = X"7" 
+				OR IR(IR_SIZE-1 DOWNTO 12) = X"8" OR IR(IR_SIZE-1 DOWNTO 12) = X"F" )
+		ELSE ONE_OPERAND WHEN IR(IR_SIZE-1 DOWNTO 12) = X"0" AND NOT (IR(11 DOWNTO 8) = X"0")
+		ELSE ZERO_OPERAND WHEN IR(IR_SIZE-1 DOWNTO 8) = X"0" AND NOT (IR(7 DOWNTO 0) = X"01")
+		ELSE BRANCH WHEN IR(IR_SIZE-1 DOWNTO 12) = X"8"
+		ELSE NO_INSTRUCTION WHEN IR = X"0001";
+		
+	-------------------------------------------------------------------------------------------	
 	----------------------------------Counters Enable Circuit----------------------------------
-	--TODO 
-	-- * ENABLE_MAIN_COUNTER after the operation finishes finishes by a CIRCUIT that GETs THE LAST BIT IN THE ROM CONTROL WORD AT NEGATIVE EDGE OF THE CONTROL CLK 
-	-- * RESET THE MAIN COUNTERS AFTER THE INSTRUCTION FINISHES by the end instruction
-	-- The enable of the main counter is cleared after every count to allow the operation to be completed to work
+	--TODO FIND OUT WHEN THE COUNTER WILL BE RESET AND THE ENABLE IS SET
+	-- The MAIN COUNTER enable is set if the control word has go back bit is set at the negative edge
+	PROCESS (CONTROL_CLK)
+	BEGIN
+		--ROM_BLOCKS_OUTPUT(0) IS THE END SIGNAL
+		IF ( RISING_EDGE(CONTROL_CLK) AND ROM_BLOCKS_OUTPUT(0) = '1' ) THEN
+		--TODO RESET THE IR REGISTER TO 0001
+			--RESET THE MAIN COUNTER
+			NEW_INSTRUCTION <= '1';
+			--SET ENABLE THE MAIN COUNTER
+			ENABLE_MAIN_COUNTER <= '1';
+		--ROM_BLOCKS_OUTPUT(1) IS THE GO BACK SIGNAL
+		ELSIF ( FALLING_EDGE(CONTROL_CLK) AND ROM_BLOCKS_OUTPUT(1) = '1' AND ROM_BLOCKS_OUTPUT(0) = '0') THEN 
+			--SET ENABLE THE MAIN COUNTER 
+			ENABLE_MAIN_COUNTER <= '1';
+		END IF;
+		  
+		IF (FALLING_EDGE(CONTROL_CLK) AND NEW_INSTRUCTION = '1') THEN 
+			--RESET THE NEW INSTRUCTION SIGNAL AT THE NEGATIVE EDGE OF THE SAME CYCLE
+			NEW_INSTRUCTION <='0';
+		END IF;
+	END PROCESS;
+	-- The MAIN COUNTER enable is cleared after every count to allow the INSTRUCTION COUNTER to count
 	PROCESS (MAIN_COUNTER_OUTPUT)
 	BEGIN
 		ENABLE_MAIN_COUNTER <= '0';
@@ -91,22 +155,9 @@ ARCHITECTURE CONTROL_UNIT OF CU IS
 		ENABLE_INSTRUCTION_COUNTER <= NOT ENABLE_MAIN_COUNTER;
 	END PROCESS;
 	
-	------------------------------------------------------------------------------------------
-	-------------------------------Decode the IR for the MODE----------------------------
-	-- Mode = 0 the IR wasn't fetched yet
-	--		= 1 2 operands
-	--		= 2 1 operands
-	--		= 3 zero operand
-	--		= 4 branch
-	MODE <= "001" WHEN NOT (IR(IR_SIZE-1 DOWNTO 12) = X"0" OR IR(IR_SIZE-1 DOWNTO 12) = X"7" 
-				OR IR(IR_SIZE-1 DOWNTO 12) = X"8" OR IR(IR_SIZE-1 DOWNTO 12) = X"F" )
-		ELSE "010" WHEN IR(IR_SIZE-1 DOWNTO 12) = X"0" AND NOT (IR(11 DOWNTO 8) = X"0")
-		ELSE "011" WHEN IR(IR_SIZE-1 DOWNTO 8) = X"0" AND NOT (IR(7 DOWNTO 0) = X"01")
-		ELSE "100" WHEN IR(IR_SIZE-1 DOWNTO 12) = X"8"
-		ELSE "000" WHEN IR = X"0001";
 	
 	-------------------------------------------------------------------------------------------
-	-------------------Operations sequence for different MODES---------------------------------
+	-------------------MAIN COUNTER sequence for different MODES---------------------------------
 	--This circuit is to calculate the operations to skip depending on the MODE and the addressing
 	--mode in case of two operand operations
 	PROCESS (MAIN_COUNTER_OUTPUT, MODE)
@@ -146,4 +197,22 @@ ARCHITECTURE CONTROL_UNIT OF CU IS
 		MAIN_COUNTER_INCREMENT <= RESULT;
 	END PROCESS;
 	
+	--------------------------------------------------------------------------------------------------
+	-----------------------------------Control Word MUX-----------------------------------------------
+	ROM_BLOCKS_OUTPUT <= 
+		INSTRUCTION_FETCHING_BLOCK_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "000" ELSE 
+		SRC_FETCHING_BLOCK_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "001" ELSE 
+		DEST_FETCHING_BLOCK_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "010" ELSE 
+		EXECUTE_BLOCK_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "011" ELSE 
+		EXECUTE_BRANCH_BLOCK_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "100";
+		
+	---------------------------------------------------------------------------------------------------
+	---------------------------------INITIAL ADDRESS MUX-----------------------------------------------
+	INITIAL_ADDRESS <= 
+		INSTRUCTION_FETCHING_BLOCK_INITIAL_ADDRESS_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "000" ELSE 
+		SRC_FETCHING_BLOCK_INITIAL_ADDRESS_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "001" ELSE 
+		DEST_FETCHING_BLOCK_INITIAL_ADDRESS_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "010" ELSE 
+		EXECUTE_BLOCK_INITIAL_ADDRESS_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "011" ELSE 
+		EXECUTE_BRANCH_BLOCK_INITIAL_ADDRESS_OUTPUT WHEN MAIN_COUNTER_OUTPUT = "100";
+		
 END CONTROL_UNIT;
